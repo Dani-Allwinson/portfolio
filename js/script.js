93bb1f517409f4abb94382748a98233ffadc3186
Dani-Allwinson <daniallwinson27@gmail.com>
2023-05-08 20:01:39 +0530

    add js file

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const menuIcon = document.getElementsByClassName('togglemenu')[0];
+const navBarLinks = document.getElementsByClassName('ul-container')[0];
+const bar1 = document.getElementById('b1');
+const bar2 = document.getElementById('b2');
+const bar3 = document.getElementById('b3');
+console.log(menuIcon);
+menuIcon.addEventListener('click',()=>{
+    navBarLinks.classList.toggle('active');
+})
